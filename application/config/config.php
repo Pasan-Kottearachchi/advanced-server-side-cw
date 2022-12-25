07e00455cee52f78c0c4955ab465f6fd32594b20
Pasan <pasankottearachchi@gmail.com>
2022-12-25 10:53:42 +0530

    feat: add production variables

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // TODO PASAN: Change this to your own URL
-$config['base_url'] = 'http://localhost/quiz-app/';
+$config['base_url'] = 'http://localhost/quiz-app/index.php/';
+//$config['base_url'] = 'https://w1761885.users.ecs.westminster.ac.uk/quiz-app/index.php/';
 
 /*
 |--------------------------------------------------------------------------
@@ -36,7 +37,7 @@ $config['base_url'] = 'http://localhost/quiz-app/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+$config['index_page'] = 'index.php';
 
 /*
 |--------------------------------------------------------------------------
