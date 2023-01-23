39a21d9a013c303e85f6af995504548219467f9d
chathurabuddi <chathu.buddi@gmail.com>
2023-01-23 07:04:20 +0530

    style: add css styles

--- application/views/navigation/navbar.php
+++ application/views/navigation/navbar.php
@@ -12,32 +12,41 @@ defined('BASEPATH') or exit('No direct script access allowed');
 	<link rel="stylesheet" type="text/css" href="<?php echo CSS_URL . 'assets/css/navigation/navbar.css'; ?>">
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light fixed-top" style="background-color: #5d7c96;">
-	<a href="<?php echo base_url('home') ?>"> <img src="<?php echo CSS_URL.'assets/img/logo.svg'; ?>" class="logo"
-		 alt="User Avatar"> </a>
-	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
-			aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-		<span class="navbar-toggler-icon"></span>
-	</button>
-	<div class="collapse navbar-collapse" id="navbarNavDropdown">
-		</ul>
-		<div class="user-account">
-			<ul class="navbar-nav">
-				<li class="nav-item dropdown">
-					<a class="nav-link dropdown-toggle navbar-user-name" href="#" id="navbardrop" data-toggle="dropdown">
-						<img src="https://www.w3schools.com/bootstrap4/img_avatar1.png" class="user-avatar"
-							 alt="User Avatar">
-						<?php echo $this->session->userdata('name'); ?>
-					</a>
-					<div class="dropdown-menu">
-						<a class="dropdown-item" href="<?php echo base_url('quiz/manage') ?>">My Quizes</a>
-						<a class="dropdown-item" href="<?php echo base_url('profile/'.$this->session->userdata('user_id')) ?>">Edit Profile</a>
-						<a class="dropdown-item" href="<?php echo base_url('logout') ?>">Logout</a>
-					</div>
-				</li>
+<nav class="navbar navbar-expand-lg navbar-light fixed-top" style="background-color: #797a9e;">
+<!--	<div class="navbar-wrapper">-->
+		<a href="<?php echo base_url('home') ?>" class="logo_a">
+			<img src="<?php echo CSS_URL.'assets/img/quzzy_time_logo.png'; ?>" class="logo" alt="User Avatar">
+		</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
+				aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+<!--			<span class="navbar-toggler-icon"></span>-->
+			<label for="check" class="toggler-icon">
+				<span></span>
+				<span></span>
+				<span></span>
+			</label>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarNavDropdown">
 			</ul>
-		</div>
+			<div class="user-account">
+				<ul class="navbar-nav">
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle navbar-user-name" href="#" id="navbardrop" data-toggle="dropdown">
+							<img src="<?php echo CSS_URL.'assets/img/avtar.png'; ?>" class="user-avatar"
+								 alt="User Avatar">
+							Hi, <?php echo $this->session->userdata('name'); ?>
+						</a>
+						<div class="dropdown-menu stay-open">
+							<a class="dropdown-item" href="<?php echo base_url('quiz/manage') ?>">My Quizes</a>
+							<a class="dropdown-item" href="<?php echo base_url('profile/'.$this->session->userdata('user_id')) ?>">Edit Profile</a>
+							<a class="dropdown-item" href="<?php echo base_url('logout') ?>">Logout</a>
+						</div>
+					</li>
+				</ul>
+			</div>
+<!--		</div>-->
 	</div>
+
 </nav>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
